84a029492cce7f4a8aed77967917741de4608dd9
ManuelFBS <manuelf.borrego@gmail.com>
2024-08-05 13:15:46 -0400

    Corrección en vite.config.js, Actualización de package.json y "/dist"...

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { config } from 'dotenv';
+
+config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: VITE_BACK_URL_DEPLOYED,
+        target: process.env.VITE_BACK_URL_DEPLOYED,
         changeOrigin: true,
         secure: true,
       },
