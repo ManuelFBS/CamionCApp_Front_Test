507ebdb17b52f7c44c3decaf8fbbb27f65903626
ManuelFBS <manuelf.borrego@gmail.com>
2024-08-05 12:53:25 -0400

    MOdificación de vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: VITE_BACK_URL_DEPLOYED,
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
 });
