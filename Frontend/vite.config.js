0f389fc2150b7bb29e0ecb717877085e61761fe3
ManuelFBS <manuelf.borrego@gmail.com>
2024-08-12 20:07:03 -0400

    Correcciones de ultima hora...

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,27 +1,27 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-// import { config } from 'dotenv';
-
-// config();
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     proxy: {
-//       '/api': {
-//         target: process.env.VITE_BACK_URL_DEPLOYED,
-//         changeOrigin: true,
-//         secure: true,
-//       },
-//     },
-//   },
-// });
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { config } from 'dotenv';
+
+config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: process.env.VITE_BACK_URL_DEPLOYED,
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
 });
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// });
